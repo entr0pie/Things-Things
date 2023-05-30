8aa76286ddb3c8461ea95d604d1f4dedac3214fe
Agostinho Carrara <tandera@notemail.com>
2023-05-30 20:52:39 -0300

    Backend SQLite3 Injection

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const app = express();
 
 const PORT = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const sqlite3 = require('sqlite3').verbose();
+const db = new sqlite3.Database('./database/database.db');
+
+app.get("/", (_, res) => {
+  res.redirect("/costumers")
+});
+
+app.get("/costumers", (req, res) => {
+  const search_query = req.query.search;
+  db.all(`SELECT * FROM customers WHERE name LIKE '%${search_query}%';`, (err, rows) => {
+    if (err) {
+      res.status(500).json({"error": err.message});
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 app.listen(PORT, () => {
