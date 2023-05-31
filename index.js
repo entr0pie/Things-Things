93c6049723821e04e027d7f6d0e22724400fd729
Agostinho Carrara <tandera@notemail.com>
2023-05-31 08:29:18 -0300

    Translation to english

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const express = require('express');
 const app = express();
 const PORT = 5000;
 
+const morgan = require('morgan');
+app.use(morgan('common'));
+
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./database/database.db');
 
